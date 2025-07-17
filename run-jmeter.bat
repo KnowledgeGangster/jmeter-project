e6d55028a0a399ba1e57ace00fa86cf86007a8cf
KnowledgeGangster <147931785+KnowledgeGangster@users.noreply.github.com>
2025-07-17 10:49:37 -0400

    Update run-jmeter.bat

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Running JMeter test...
+if not exist results mkdir results
 "C:\apache-jmeter-5.6.3\bin\jmeter.bat" -n -t test-plan.jmx -l results/results.jtl -e -o results/html
 echo Test finished. Report is in results/html
-pause
