875d743ffcdab4ed2d1269bc419992cbb3b5a6de
KnowledgeGangster <147931785+KnowledgeGangster@users.noreply.github.com>
2025-07-17 10:42:19 -0400

    Update run-jmeter.bat

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -1,4 +1,9 @@
+@echo off
+set JMETER_HOME=C:\apache-jmeter-5.6.3
+set PATH=%JMETER_HOME%\bin;%PATH%
+
 echo Running JMeter test...
-"C:\apache-jmeter-5.6.3\bin\jmeter.bat" -n -t Learnix.jmx -l results/result.jtl -e -o results/html
-echo Test finished. Report is in results/html
-pause
+"%JMETER_HOME%\bin\jmeter.bat" -n -t Learnix.jmx -l results\result.jtl -e -o results\html
+
+echo Test finished. Report is in results\html
+exit /b 0
