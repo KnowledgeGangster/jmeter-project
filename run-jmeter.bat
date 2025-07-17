e2086e487469f16f7e2a4dc1557ddad97947d094
KnowledgeGangster <147931785+KnowledgeGangster@users.noreply.github.com>
2025-07-17 11:15:04 -0400

    Update run-jmeter.bat

--- run-jmeter.bat
+++ run-jmeter.bat
@@ -1,13 +1,19 @@
 @echo off
 echo Running JMeter test...
 
+REM Delete existing results.jtl file if it exists
+IF EXIST results\results.jtl (
+    echo Deleting existing results\results.jtl file...
+    del /q results\results.jtl
+)
+
 REM Delete existing report folder if it exists
 IF EXIST results\html (
     echo Deleting existing results\html folder...
     rmdir /s /q results\html
 )
 
-REM Run the JMeter test and generate the HTML report
+REM Run JMeter test
 "C:\apache-jmeter-5.6.3\bin\jmeter.bat" -n -t test-plan.jmx -l results\results.jtl -e -o results\html
 
 echo Test finished. Report is in results\html
